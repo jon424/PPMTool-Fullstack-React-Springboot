518d665c94c119aa5fca4e4ca0313bab19445db0
Jon Jackson <jon.jackson@perficient.com>
2022-01-06 16:42:10 -0500

    adding to branch1

---
+++ ppmtool/src/main/java/com/jonjackson/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.jonjackson.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//extend the CrudRepository, passing in the Project object and the type of ID (Long)
+//CrudRepository gives us a set of methods to use out of the box
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
