7e65426614563be51cc6b71089180406c156a1ed
Jon Jackson <jon.jackson@perficient.com>
2022-01-26 20:51:55 -0500

    set up redux store

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -25,75 +25,70 @@ const AddProject = (props) => {
   };
 
   return (
-    <>
-      <div className="register">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h5 className="display-4 text-center">Create Project form</h5>
-              <hr />
-              <form onSubmit={onSubmit}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg "
-                    placeholder="Project Name"
-                    name="projectName"
-                    value={projectName}
-                    onChange={(e) => setProjectName(e.target.value)}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Unique Project ID"
-                    name="projectIdentifier"
-                    value={projectIdentifier}
-                    onChange={(e) => setProjectIdentifier(e.target.value)}
-                  />
-                </div>
-
-                <div className="form-group">
-                  <textarea
-                    className="form-control form-control-lg"
-                    placeholder="Project Description"
-                    name="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                  ></textarea>
-                </div>
-                <h6>Start Date</h6>
-                <div className="form-group">
-                  <input
-                    type="date"
-                    className="form-control form-control-lg"
-                    name="start_date"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  />
-                </div>
-                <h6>Estimated End Date</h6>
-                <div className="form-group">
-                  <input
-                    type="date"
-                    className="form-control form-control-lg"
-                    name="end_date"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                  />
-                </div>
-
+    <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h5 className="display-4 text-center">Create Project form</h5>
+            <hr />
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
                 <input
-                  type="submit"
-                  className="btn btn-primary btn-block mt-4"
+                  type="text"
+                  className="form-control form-control-lg "
+                  placeholder="Project Name"
+                  name="projectName"
+                  value={projectName}
+                  onChange={(e) => setProjectName(e.target.value)}
                 />
-              </form>
-            </div>
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Unique Project ID"
+                  name="projectIdentifier"
+                  value={projectIdentifier}
+                  onChange={(e) => setProjectIdentifier(e.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  className="form-control form-control-lg"
+                  placeholder="Project Description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <h6>Start Date</h6>
+              <div className="form-group">
+                <input
+                  type="date"
+                  className="form-control form-control-lg"
+                  name="start_date"
+                  value={startDate}
+                  onChange={(e) => setStartDate(e.target.value)}
+                />
+              </div>
+              <h6>Estimated End Date</h6>
+              <div className="form-group">
+                <input
+                  type="date"
+                  className="form-control form-control-lg"
+                  name="end_date"
+                  value={endDate}
+                  onChange={(e) => setEndDate(e.target.value)}
+                />
+              </div>
+
+              <input type="submit" className="btn btn-primary btn-block mt-4" />
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ ppmtool-react-client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  //contains all reducers we are creating as we move forward
+});
