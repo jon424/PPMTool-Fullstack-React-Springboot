a83b95f6ccc355c49cde42ee549df78044bef76f
Jon Jackson <jon.jackson@perficient.com>
2022-02-17 15:45:50 -0500

    added new classes Backlog and ProjectTask

---
+++ ppmtool/src/main/java/com/jonjackson/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.jonjackson.ppmtool.repositories;
+
+import com.jonjackson.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
---
+++ ppmtool/src/main/java/com/jonjackson/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.jonjackson.ppmtool.repositories;
+
+import com.jonjackson.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
